5b0305073e83f00cb22d7e67c6fa7a996a8fca4b
Robin Scholz <rbnschlz@gmail.com>
2017-12-20 12:49:42 +0100

    Clean up

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -31,48 +31,19 @@
     },
     data () {
       return {
-        // idleStatus: false,
-        // pageNumber: -1,
         topScroll: 0
-        // idleNumOne: 0,
-        // idleNumTwo: 0
       }
     },
-    // onIdle () {
-    //   this.idleStatus = true
-    //   // this.start()
-    // },
-    // onActive () {
-    //   this.stop()
-    // },
     methods: {
       ...mapMutations([
         'SAVE_SCROLL',
         'SHOW_CAPTIONS',
         'SHIVER'
       ]),
-      // start () {
-      //   this.interval = setInterval(function () {
-      //     this.idleNumOne = Math.floor(Math.random() * this.imgnum) + 1
-      //     this.idleNumTwo = Math.floor(Math.random() * 8)
-      //   }.bind(this), 10000)
-      // },
-      // stop () {
-      //   clearInterval(this.interval)
-      // },
       scrollWatch () {
         this.topScroll = window.pageYOffset || document.documentElement.scrollTop
         this.SAVE_SCROLL(this.topScroll)
       }
-    },
-    watch: {
-      // idleStatus () {
-      //   if (this.idleStatus) {
-      //     this.start()
-      //   } else {
-      //     this.stop()
-      //   }
-      // }
     },
     mounted () {
       window.addEventListener('scroll', this.scrollWatch)
